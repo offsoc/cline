7cf1a2201002c4dc1957a06ec591902c3c9fbce9
canvrno <kevin@cline.bot>
2025-06-25 02:35:48 -0700

    cleanup

--- src/core/task/index.ts
+++ src/core/task/index.ts
@@ -907,8 +907,6 @@ export class Task {
 		this.mcpHub.clearNotificationCallback()
 	}
 
-	// Checkpoints logic moved to checkpointManager - call directly on this.checkpointManager
-
 	// Tools
 
 	/**
